28888ff9ac84ce636a21b25a4de458096944e378
myliang <liangyuliang0335@gmail.com>
2012-11-21 11:38:36 +0800

    Update config/initializers/mongo.rb
    modify counter_cache

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -28,23 +28,17 @@ module MongoMapper
 
         def setup_with(model)
           setup_without(model)
-          puts ":::::class==#{model}"
-          puts ":::::options==#{options.to_json}"
-          puts "::counter_cache::#{options[:counter_cache]}"
-          # counter_count
+          # counter_cache
           if counter_cache?
-            counter = "#{model.to_s.tableize}_count"
-            puts ":::#{counter}::: field::: #{type_key_name}"
-            puts ":::klass:::#{model.associations[name.inspect]}"
-            klass.key counter, default: 0
-            # add after save method increment
-            model.after_save do 
-              send(name).send :increment, counter.to_sym, 1
-              # send(name).send "#{counter}=", (model.send counter + 1)
+            # counter = "#{model.to_s.tableize}_count"
+            model.after_save do |m|
+              association = m.send(name)
+              association.class.key counter_name unless association.class.key?(counter_name)
+
+              m.send(name).increment("#{counter_name}": 1)
             end
-            model.after_destroy do
-              send(name).send :decrement, counter.to_sym, 1
-              # send(name).send "#{counter}=", (model.send counter - 1)
+            model.after_destroy do |m|
+              m.send(name).decrement("#{counter_name}": 1)
             end
           end
         end
@@ -54,6 +48,9 @@ module MongoMapper
 
         def counter_cache?
           options[:counter_cache]
+        end
+        def counter_name
+          "#{@model.to_s.tableize}_count"
         end
       end
     end
