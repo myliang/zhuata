82487690445e8947d882ccd9e597c2a4ba11ec22
myliang <liangyuliang0335@gmail.com>
2012-11-28 22:39:43 +0800

    add test contentController

---
+++ app/controllers/content_controller.rb
@@ -0,0 +1,66 @@
+class ContentController < ApplicationController
+
+  before_filter :build_model, :except => [:index, :tag]
+  before_filter :authenticate_user!, :except => [:index, :tag]
+
+  def index
+    instance_model_names_set model_class.page(build_params)
+  end
+
+  def tag
+    index
+    render :index
+  end
+
+  def new; end
+
+  def show 
+    instance_model_name.update_read_counter
+    @comment = Comment.new(:commentable => instance_model_name)
+  end
+
+  def edit; end
+
+  def create
+    instance_model_name_set model_class.new(params[model_name])
+    instance_model_name.user = current_user
+    instance_model_name.save
+    respond_with instance_model_name, :location => blogs_url
+  end
+
+  def update
+    instance_model_name.old_tags = instance_model_name.tags
+    instance_model_name.update_attributes(params[model_name])
+    respond_with instance_model_name, :location => blogs_url
+  end
+
+  def build_model
+    instance_model_name_set model_class.find(params[:id]) if params[:id]
+    instance_model_name_set model_class.new unless instance_model_name
+  end
+
+  def build_params
+    hash = {:order => :created_at.desc}
+    params.each do |k, v|
+      hash[k.to_sym] = v unless ["controller", "action"].include?(k)
+    end
+    hash
+  end
+
+  def model_class
+    controller_name.classify.constantize
+  end
+  def model_name
+    controller_name.singularize
+  end
+  def instance_model_name
+    instance_variable_get("@#{model_name}")
+  end
+  def instance_model_name_set(v)
+    instance_variable_set("@#{model_name}", v)
+  end
+  def instance_model_names_set(v)
+    instance_variable_set("@#{controller_name}", v)
+  end
+
+end
--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -2,11 +2,66 @@ require 'spec_helper'
 
 describe BlogsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  let(:blog){ mock_model(Blog) }
+
+  describe "protected methods" do
+    let(:controller) { BlogsController.new }
+
+    it "model_class should be Blog" do
+      controller.model_class.should == Blog
     end
+
+    it "model_name should be blog" do
+      controller.model_name.should == "blog"
+    end
+
+    it "instance_model_name should be nil" do
+      controller.instance_model_name.should be_nil
+    end
+
+    it "instance_model_name should be " do
+      controller.instance_model_name_set(blog)
+      controller.instance_model_name.should eq blog
+    end
+
+  end
+
+  describe "GET 'index'" do
+    context "no params" do
+
+      before :each do
+        Blog.should_receive(:page).and_return([])
+        controller.should_not_receive(:build_model)
+        get 'index'
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "should render index" do
+        response.should render_template("index")
+      end
+
+      it "assigns blog should be empty" do
+        assigns[:blogs].should be_empty
+      end
+
+      it "build_params should have key :order " do
+        controller.build_params.should have_key(:order)
+      end
+    end
+
+    context "params with tags: ruby" do
+      before :each do
+        get 'index', {tags: "ruby"}
+      end
+
+      it "build_params should have key :tags" do
+        controller.build_params.should have_key(:tags)
+      end
+    end
+
   end
 
 end
