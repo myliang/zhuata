3324197dcfbd4b702e779864061b5318baa9e5ab
myliang <liangyuliang0335@gmail.com>
2013-03-19 22:09:26 +0800

    shi shi

--- app/models/audio_book.rb
+++ app/models/audio_book.rb
@@ -12,10 +12,24 @@ class AudioBook
   many :chapters
 
   before_create do |entity|
+    entity.chapters = []
+
+    # http://www.5tps.com
     doc = Spider::Http.get(url)
-    doc.css('#bfdz ul li a').each do |link|
-      "http://www.5tps.com#{link.attr('href')}"
-      link.content
+    entity.title = doc.css('#i>b>h1')[0].text
+    doc.css('.ny_txt > ul').each do |ul|
+      ps = ul.css('>span>p')
+      entity.tags = ps[0].text.split(':')[1]
+      entity.talker = ps[1].text.split(':')[1]
+      entity.text = ul.css('>p')[0].text
+    end
+    doc.css('#bfdz ul li a:first-child').each do |link|
+      chapter = Chapter.new
+      chapter.url = "http://www.5tps.com#{link.attr('href')}"
+      chapter.title = link.content
+      chapter.number = chapter.title.scan(/\d+/)[0].to_i
+
+      entity.chapters << chapter
     end
   end
 
