5ec71ce99a2a279da1a16cf7a360340d298b4b50
myliang <liangyuliang0335@gmail.com>
2013-04-24 22:15:31 +0800

    rewrite css

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,9 @@ set :whenever_command, "bundle exec whenever"
 
 
 # 设置path
-set :default_environment, do
-    "PATH" => "/opt/ruby1.8.7/bin:$PATH"
-end
+set :default_environment, { "PATH" => "/opt/ruby1.8.7/bin:$PATH" }
 
-set :application, "top3c"
+set :application, "zhuata"
 set :repository,  "git@github.com:myliang/zhuata.git"
 set :keep_releases, 5 #只保留5个备份
 
@@ -38,13 +36,13 @@ role :db,  "115.100.249.73" , :primary => true
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-    # task :start  do ; end
-    # task :stop  do ; end
-    # task :restart, :roles => :app, :except => { :no_release => true } do
-        # run "touch #{File.join(current_path,'tmp','restart.txt')}"
-    # end
-# end
+namespace :deploy do
+  task :start  do ; end
+  task :stop  do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 #
 desc "Copy shared config files to current application."
 task :after_update_code, :roles => :app do
